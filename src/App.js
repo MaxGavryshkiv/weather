da54646fe9fc81562fe4f5ef0ea4e845addc81d0
Max Gavryshkiv <maks20355@gmail.com>
2023-12-24 22:58:38 +0200

    change routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  createHashRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import SearchPage from "./views/SearchPage";
 import WeatherPage from "./views/WeatherPage";
 
 function App() {
+  const router = createHashRouter([
+    {
+      path: "/*",
+      element: <SearchPage />,
+    },
+    {
+      path: "/weather",
+      element: <WeatherPage />,
+    },
+  ]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<SearchPage />}></Route>
-        <Route path="/weather" element={<WeatherPage />}></Route>
-      </Routes>
+      <HashRouter>
+        <RouterProvider router={router} />
+      </HashRouter>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+{
+  /* <Route exact path="/" element={<SearchPage />}></Route>
+        <Route path="/weather" element={<WeatherPage />}></Route> */
+}
