8b7a09de9f7f5a5a84598307de0f5fe75b35dbcf
Max Gavryshkiv <maks20355@gmail.com>
2023-12-24 23:05:30 +0200

    get back routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter,
-  HashRouter,
-  createHashRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchPage from "./views/SearchPage";
 import WeatherPage from "./views/WeatherPage";
 
 function App() {
-  const router = createHashRouter([
-    {
-      path: "/",
-      element: <SearchPage />,
-    },
-    {
-      path: "/weather",
-      element: <WeatherPage />,
-    },
-  ]);
-
   return (
     <BrowserRouter>
-      <HashRouter>
-        <RouterProvider router={router} />
-      </HashRouter>
+      <Routes>
+        <Route exact path="/" element={<SearchPage />}></Route>
+        <Route path="/weather" element={<WeatherPage />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-{
-  /* <Route exact path="/" element={<SearchPage />}></Route>
-        <Route path="/weather" element={<WeatherPage />}></Route> */
-}
