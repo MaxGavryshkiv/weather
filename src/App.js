7c864f53c6e4b3b7e5b74b70597adb63c4a45b9b
Max Gavryshkiv <maks20355@gmail.com>
2023-12-24 23:06:58 +0200

    Try HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route } from "react-router-dom";
 import SearchPage from "./views/SearchPage";
 import WeatherPage from "./views/WeatherPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <HashRouter>
         <Route exact path="/" element={<SearchPage />}></Route>
         <Route path="/weather" element={<WeatherPage />}></Route>
-      </Routes>
+      </HashRouter>
     </BrowserRouter>
   );
 }
